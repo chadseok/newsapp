4a86b240bb437af05ec841ef0b2d269e569ff59d
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-09 14:25:41 +0900

    Main페이지의 news의 카테고리 네비게이션에 대한 리스트 배열 지정

---
+++ src/lib/constants/main.ts
@@ -0,0 +1,9 @@
+export const categoryList = [
+  { id: "general", name: "일반" },
+  { id: "business", name: "비즈니스" },
+  { id: "health", name: "건강" },
+  { id: "science", name: "과학" },
+  { id: "technology", name: "기술" },
+  { id: "entertainment", name: "연예" },
+  { id: "sports", name: "스포츠" },
+] as const;
