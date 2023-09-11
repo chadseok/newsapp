325be7854cb14485656d70c5b7e52b06598e8cec
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-11 18:29:32 +0900

    모든 페이지 감싸는 wrapper div에 대한 스타일 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Router from "@/routes/Router";
 
 const App = () => {
-  return <Router />;
+  return (
+    <div className="w-full min-h-screen pt-12 bg-slate-100">
+      <Router />
+    </div>
+  );
 };
 
 export default App;
