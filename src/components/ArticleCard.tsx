951ad7d80e92e3c3958c788964bc92a14675f932
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-11 18:31:05 +0900

    Main페이지에 들어갈 뉴스 카드 컴포넌트 생성

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import type { Article } from "@/lib/types/main";
+
+type Props = { data: Article };
+
+const ArticleCard = ({ data }: Props) => {
+  return (
+    <div className="bg-white rounded-md shadow-md overflow-hidden col-span-1 row-span-2">
+      <Link to={data.url} target="_blank" rel="noreferrer">
+        <div className="flex flex-col justify-between h-full">
+          <img className="w-full h-52 object-cover" src={data.urlToImage} />
+          <div className="p-2">
+            <span className="text-xs">{data.source.name}</span>
+            <h2 className="font-bold text-sm h-16 mt-2 text-linecut-3">
+              {data.title}
+            </h2>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ArticleCard;
