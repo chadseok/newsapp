4e902c89a00b1a0884d4940307c7aba560ad94d7
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-15 10:27:41 +0900

    searchLoader 기능 구현 및 Router와 연결

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "@/pages/Home";
 import Search from "@/pages/Search";
-import { homeLoader } from "./loader";
+import { homeLoader, searchLoader } from "./loader";
 
 const routerObject = createBrowserRouter([
   { path: "/", element: <Home />, loader: homeLoader },
-  { path: "/search", element: <Search /> },
+  { path: "/search", element: <Search />, loader: searchLoader },
 ]);
 
 const Router = () => {
--- src/routes/loader.ts
+++ src/routes/loader.ts
@@ -1,6 +1,6 @@
 import type { LoaderFunction } from "react-router-dom";
-import { getToptrendArticlesApi } from "@/api/main";
-import type { Category, QueryObject } from "@/lib/types/main";
+import { getToptrendArticlesApi, getSearchArticlesApi } from "@/api/main";
+import type { Category, SearchQuery, TopHeadlineQuery } from "@/lib/types/main";
 
 export const homeLoader: LoaderFunction = async ({ request }) => {
   const category =
@@ -8,7 +8,7 @@ export const homeLoader: LoaderFunction = async ({ request }) => {
     "general";
   const page = Number(new URL(request.url).searchParams.get("page") || 1);
 
-  const queryObject: QueryObject = {
+  const queryObject: TopHeadlineQuery = {
     apiKey: import.meta.env.VITE_NEWS_API_KEY,
     country: "kr",
     category,
@@ -19,5 +19,22 @@ export const homeLoader: LoaderFunction = async ({ request }) => {
   const res = await getToptrendArticlesApi(queryObject);
   const body = await res.json();
 
-  return { list: body.articles, page, category };
+  return body.articles;
+};
+
+export const searchLoader: LoaderFunction = async ({ request }) => {
+  const search = new URL(request.url).searchParams.get("q");
+  const page = Number(new URL(request.url).searchParams.get("page") || 1);
+
+  const queryObject: SearchQuery = {
+    apiKey: import.meta.env.VITE_NEWS_API_KEY,
+    q: search || "",
+    page,
+    pageSize: 12,
+  };
+
+  const res = await getSearchArticlesApi(queryObject);
+  const body = await res.json();
+
+  return body.articles;
 };
