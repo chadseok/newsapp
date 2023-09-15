e1559fd2f8fe888df7359be2795e3e09b82f89fe
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-15 17:53:52 +0900

    Header 로고에 메인 페이지 링크 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
     <div className="w-full bg-blue-500 shadow-md">
       <header className="flex justify-between items-center  w-[60rem] h-12 mx-auto px-2 ">
-        <div className="text-xl text-white font-bold">📰 NEWSAPP</div>
+        <h1 className="text-xl text-white font-bold">
+          <Link to="/">📰 NEWSAPP</Link>
+        </h1>
         <SearchBar />
       </header>
     </div>
